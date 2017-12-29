c60499e0121f013e96471ae5176bfa142690898a
TheChugnut <samwarner.uk@gmail.com>
2017-12-29 15:39:50 +0000

    Created functions

--- jde_dates.py
+++ jde_dates.py
@@ -1,18 +1,19 @@
 from datetime import datetime, date
 
-class JdeDates():
+class JdeDates:
     """ 
-    Creates an object which accepts either a julian date or 
-    a standard date, and then converts it to the other format.
+    Creates an object which accepts either a JDE Julian date (CYYDDD) or 
+    a standard ISO date (YYYY-MM-DD), and then converts it to the other
+    format, storing both as attributes.
     """
     def __init__(self, value_in):
         self.value_in = value_in
         self.iso_date = None
         self.julian_date = None
-        self.convert()
+        self._convert()
         
-    def convert(self):
-        # test if date object and apply the approprate conversion
+    def _convert(self):
+        # Test if date object and apply the approprate conversion
         if isinstance(self.value_in, date):
             self.iso_date = self.value_in
             self.julian_date = int(self.value_in.strftime(
@@ -23,3 +24,11 @@ class JdeDates():
                 str(int(self.value_in) + 1900000), '%Y%j'))
 
 
+
+def convert_to_julian(iso_date):
+    return int(iso_date.strftime('%Y%j')) - 1900000
+
+
+def convert_to_iso(julian_date):
+    return datetime.date(datetime.strptime(
+        str(int(julian_date) + 1900000), '%Y%j'))
